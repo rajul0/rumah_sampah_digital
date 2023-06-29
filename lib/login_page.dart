b18c52ab2156514af40a643b3cfb2b3fdd393d30
rajul0 <sirajulilmi@gmail.com>
2023-06-29 20:04:57 +0700

    buat halaman login

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -8,6 +8,8 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,19 +19,108 @@ class _LoginPageState extends State<LoginPage> {
             left: 47.0,
             right: 47.0,
           ),
-          child: Column(children: [
-            SizedBox(
-              height: 120.0,
-            ),
-            Text('Masuk',
-                textAlign: TextAlign.center,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 120.0,
+              ),
+              Align(
+                alignment: Alignment.center,
+                child: Text(
+                  'Masuk',
+                  style: TextStyle(
+                    color: Color(0xFF000000),
+                    fontSize: 38,
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 162.0,
+              ),
+              Form(
+                key: _formKey,
+                child: Column(children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'No.Hp',
+                      labelStyle: TextStyle(
+                          color: Color(0xFF000000),
+                          fontSize: 14.0,
+                          fontFamily: 'Poppins'),
+                      hintText: '0812xxxxxxxx',
+                      hintStyle: TextStyle(
+                          color: Color(0xFF000000).withOpacity(0.3),
+                          fontSize: 14.0,
+                          fontFamily: 'Poppins'),
+                      contentPadding: EdgeInsets.only(
+                        top: 13.0,
+                        bottom: 12.0,
+                        left: 18.0,
+                      ),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(6.0)),
+                          borderSide: BorderSide(color: Color(0xFF008305))),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(6.0)),
+                        borderSide: BorderSide(color: Color(0xFF008305)),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(6.0)),
+                        borderSide: BorderSide(color: Color(0xFF008305)),
+                      ),
+                    ),
+                    style: TextStyle(
+                      fontSize: 14.0,
+                      color: Color(0xFF000000),
+                      fontFamily: 'Poppins',
+                    ),
+                  )
+                ]),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'No.Hp',
+                  labelStyle: TextStyle(
+                      color: Color(0xFF000000),
+                      fontSize: 14.0,
+                      fontFamily: 'Poppins'),
+                  hintText: '0812xxxxxxxx',
+                  hintStyle: TextStyle(
+                      color: Color(0xFF000000).withOpacity(0.3),
+                      fontSize: 14.0,
+                      fontFamily: 'Poppins'),
+                  contentPadding: EdgeInsets.only(
+                    top: 13.0,
+                    bottom: 12.0,
+                    left: 18.0,
+                  ),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(6.0)),
+                      borderSide: BorderSide(color: Color(0xFF008305))),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(6.0)),
+                    borderSide: BorderSide(color: Color(0xFF008305)),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(6.0)),
+                    borderSide: BorderSide(color: Color(0xFF008305)),
+                  ),
+                ),
                 style: TextStyle(
+                  fontSize: 14.0,
                   color: Color(0xFF000000),
-                  fontSize: 38,
                   fontFamily: 'Poppins',
-                  fontWeight: FontWeight.bold,
-                ))
-          ]),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
